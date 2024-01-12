a9fb924457f0723cfb67819dca6c0a318bfbd537
evechen1226 <eveliving2021@gmail.com>
2024-01-12 16:22:14 +0800

    feat:front/main.php--增加預告片畫面及清單，清單移動按鈕功能

--- front/main.php
+++ front/main.php
@@ -1,6 +1,6 @@
 <style>
     .lists {
-        position: relative;
+        /* position: relative; */
         left: 114px;
     }
 
@@ -25,11 +25,51 @@
     .item div {
         text-align: center;
     }
-    .left, .right{
+
+    /* 下方箭頭用CSS繪製 */
+    .left,
+    .right {
         width: 0;
         border: 20px solid black;
-        border-top-color:transparent;
+        border-top-color: transparent;
         border-bottom-color: transparent;
+    }
+
+    .left {
+        border-left-width: 0;
+    }
+
+    .right {
+        border-left-width: 0;
+    }
+
+    .btns {
+        width: 360px;
+        height: 100px;
+        display: flex;
+        overflow: hidden;
+    }
+
+    .btn img {
+        width: 60px;
+        height: 80px;
+    }
+
+    .btn {
+        font-size: 12px;
+        text-align: center;
+        flex-shrink: 0;
+        width: 90px;
+    }
+
+    .controls {
+        width: 420px;
+        height: 100px;
+        position: relative;
+        margin-top: 10px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
     }
 </style>
 <div class="half" style="vertical-align:top;">
@@ -50,13 +90,40 @@
             ?>
         </div>
         <div class="controls">
+            <div class="left"></div>
+            <div class="btns"></div>
         </div>
     </div>
 </div>
 </div>
 <script>
     // .eq() 位置
-    $(".item").eq(0). show();
+    $(".item").eq(0).show();
+    // $(".btn").lengthi計算有幾個.btn
+    let total = $(".btn").length
+    // 類似分頁功能
+    // 參考題組一 校園映像檔
+    let p = 0
+    $(".left,.right").on("click", function() {
+        // 原本就有的屬性，可以直接利用
+        let arrow = $(this).attr('class')
+
+        switch (arrow) {
+            case "right":
+                if (p + 1 <= (total - 4)) {
+                    p = p + 1
+                }
+                break;
+            case "left":
+                if (p - 1 >= 0) {
+                    p = p - 1
+                }
+                break;
+        }
+        $(".btn").animate({
+            right: 90 * p
+        })
+    })
 </script>
 <style>
     .movies {
@@ -98,12 +165,12 @@
                     </a>
                 </div>
                 <div style="width:65%">
-                <div><?= $movie['name']; ?></div>
-                <div style="font-size:13px;">
-                    分級:<img src="./icon/03C0<?= $movie['level'] ?>.png" style="width:20%">
-                </div>
+                    <div><?= $movie['name']; ?></div>
+                    <div style="font-size:13px;">
+                        分級:<img src="./icon/03C0<?= $movie['level'] ?>.png" style="width:20%">
+                    </div>
 
-                <div style="font-size:13px;">上映日期:<?= $movie['ondate'] ?></div>
+                    <div style="font-size:13px;">上映日期:<?= $movie['ondate'] ?></div>
                 </div>
                 <div style="width:100%">
                     <button onclick="location.href='?do=intro$id=<?= $movie['id']; ?>'">劇情介紹</button>
@@ -114,23 +181,23 @@
             <?php
             ?>
         </div>
-        <div class="ct"> 
-<?php
-if($now-1>0){
-    $prev=$now-1;
-    echo "<a href='?p=$prev> < </a>";
-}
+        <div class="ct">
+            <?php
+            if ($now - 1 > 0) {
+                $prev = $now - 1;
+                echo "<a href='?p=$prev> < </a>";
+            }
 
-for($i=1;$i<=$pages; $i++){
-    echo "<a href='?p=$i'> $i </a>";
-}
+            for ($i = 1; $i <= $pages; $i++) {
+                echo "<a href='?p=$i'> $i </a>";
+            }
 
-if($now+1<=$pages){
-    $next=$now+1;
-    echo "<a href='?p=$next> > </a>";
-}
+            if ($now + 1 <= $pages) {
+                $next = $now + 1;
+                echo "<a href='?p=$next> > </a>";
+            }
 
-?>
+            ?>
 
         </div>
     </div>
