0e23daf0c2af66575e2fea1378556bb50b228291
evechen1226 <eveliving2021@gmail.com>
2024-01-15 13:51:52 +0800

    api/get_dates.php--修改程式碼，減少迴圈數，提高效能

--- api/get_dates.php
+++ api/get_dates.php
@@ -2,18 +2,35 @@
 
 $movie=$_GET['id'];
 
-$ondate=$Movie->find($movie)['ondate'];
+//$ondate=$Movie->find($movie)['ondate'];
+$ondate=strtotime($Movie->find($movie)['ondate']);
+
+//$end = strtotime("+2 days",strtotime($ondate));
+$end = strtotime("+2 days",$ondate);
 
 $today=date("Y-m-d");
+$dif=($end-strtotime($today))/(60*60*24);
 // ondate    today 3
 // ondate+1  today 2
 // ondate+2  today 1
 
-for($i=0;$i<3;$i++){
-    $date=strtotime("+$i days",strtotime(($ondate)));
-    if($date>=strtotime($today)){
-        $str=date("Y-m-d",$date);
-        echo "<option value='{$str}'>{$str}</option>";
+// 在迴圈內判斷
+// for($i=0;$i<3;$i++){
+//     $date=strtotime("+$i days",strtotime(($ondate)));
+//     if($date>=strtotime($today)){
+//         $str=date("Y-m-d",$date);
+//         echo "<option value='{$str}'>{$str}</option>";
 
-    }
+//     }
+// }
+
+// 在先計算相差幾天，決定迴圈數, 提高效能
+for($i=0;$i<=$diff;$i++){
+    $date=strtotime("Y-m-d",strtotime("+$i days",$ondate));
+        echo "<option value='{$date}'>{$date}</option>";
 }
+
+// for($i=(2-$diff);$i<3;$i++){
+//     $date=strtotime("Y-m-d",strtotime("+$i days",strtotime($ondate)));
+//         echo "<option value='{$date}'>{$date}</option>";
+// }
